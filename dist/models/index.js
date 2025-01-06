7a726f1613f75fa646fb02b8844ff5dce315e66c
jemanuelpo <jpachecho@libgot.com>
2025-01-06 15:11:46 -0300

    Add auto-generated API files for WhatsApp integration
    Introduced TypeScript definitions and JavaScript implementations for the WhatsApp API. These files were auto-generated using Swagger Codegen and include functionalities like finding contacts, checking WhatsApp numbers, and managing messages. Modifications to these files should be avoided as they are autogenerated.

---
+++ dist/models/index.js
@@ -0,0 +1,32 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./contact-dto"), exports);
+__exportStar(require("./inline-response200"), exports);
+__exportStar(require("./inline-response2001"), exports);
+__exportStar(require("./inline-response2001-messages"), exports);
+__exportStar(require("./inline-response2001-messages-key"), exports);
+__exportStar(require("./inline-response2001-messages-message"), exports);
+__exportStar(require("./inline-response2001-messages-message-image-message"), exports);
+__exportStar(require("./inline-response2001-messages-message-image-message-context-info"), exports);
+__exportStar(require("./inline-response2001-messages-message-text"), exports);
+__exportStar(require("./inline-response2001-messages-message-video-message"), exports);
+__exportStar(require("./inline-response2001-messages-records"), exports);
+__exportStar(require("./inline-response201"), exports);
+__exportStar(require("./inline-response2-xx"), exports);
+__exportStar(require("./inline-response500"), exports);
+__exportStar(require("./inline-response500-response"), exports);
+__exportStar(require("./where-find-contacts-dto"), exports);
+__exportStar(require("./where-find-contacts-dto-where"), exports);
+__exportStar(require("./where-find-messages-dto"), exports);
+__exportStar(require("./where-find-messages-dto-key"), exports);
+__exportStar(require("./where-find-status-message-dto"), exports);
